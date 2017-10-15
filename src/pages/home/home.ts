60af50f02be1aa62e298632b91bcba7dfdfd3ffe
austintedwards <austintedwards@gmail.com>
2017-10-15 14:56:45 -0600

    first commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import mapboxgl from 'mapbox-gl/dist/mapbox-gl.js'
+
 
 @Component({
   selector: 'page-home',
@@ -10,5 +12,20 @@ export class HomePage {
   constructor(public navCtrl: NavController) {
 
   }
+  ionViewDidLoad() {
+
+   /*Initializing Map*/
+  mapboxgl.accessToken = '<your mapbox gl js access token>';
+  var map = new mapboxgl.Map({
+  style: 'mapbox://styles/mapbox/light-v9',
+  center: [-74.0066, 40.7135],
+  zoom: 16,
+  pitch: 80,
+  minZoom: 7.5, //restrict map zoom - buildings not visible beyond 13
+  maxZoom: 17,
+  container: 'map'
+});
+
+}
 
 }
