48a0a8faebe26b9a0fbf0740ad5a85b46c760d70
austintedwards <austintedwards@gmail.com>
2017-10-15 20:42:50 -0600

    moving bus

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -17,15 +17,35 @@ export class HomePage {
    /*Initializing Map*/
    const ACCESS_TOKEN = 'pk.eyJ1IjoiYXVzdGtlIiwiYSI6ImNpd21uZTB1bDAwNm8yenF4ZmtlbjkzenUifQ.CohFKxWoYGrFXQDoRvZWag';
    mapboxgl.accessToken = ACCESS_TOKEN;
-  var map = new mapboxgl.Map({
-  style: 'mapbox://styles/mapbox/light-v9',
-  center: [-74.0066, 40.7135],
-  zoom: 16,
-  pitch: 80,
-  minZoom: 7.5, //restrict map zoom - buildings not visible beyond 13
-  maxZoom: 17,
-  container: 'map'
-});
+   var map = new mapboxgl.Map({
+       container: 'map',
+       style: 'mapbox://styles/mapbox/streets-v9',
+       zoom: 0
+   });
+
+   var url = 'https://wanderdrone.appspot.com/';
+   map.on('load', function () {
+       window.setInterval(function() {
+           map.getSource('movement').setData(url);
+       }, 50);
+
+       map.loadImage('../assets/icon/bus-top-view.png', function(error, image) {
+         if (error) throw error;
+       map.addSource('movement', { type: 'geojson', data: url });
+       console.log(map)
+       map.addImage('bus', image);
+       map.addLayer({
+           "id": "movement",
+           "type": "symbol",
+           "source": "movement",
+           "layout": {
+               "icon-image": "bus",
+               "icon-size": 0.15
+
+           }
+       });
+   });
+ });
 
 }
 
