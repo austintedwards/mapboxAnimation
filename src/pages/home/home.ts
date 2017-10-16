ec4bbcd06a46cc7a5474dc3402aa217ecfd4bb9a
austintedwards <austintedwards@gmail.com>
2017-10-16 08:55:16 -0600

    buses are moving

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -14,39 +14,39 @@ export class HomePage {
   }
   ionViewDidLoad() {
 
-   /*Initializing Map*/
-   const ACCESS_TOKEN = 'pk.eyJ1IjoiYXVzdGtlIiwiYSI6ImNpd21uZTB1bDAwNm8yenF4ZmtlbjkzenUifQ.CohFKxWoYGrFXQDoRvZWag';
-   mapboxgl.accessToken = ACCESS_TOKEN;
-   var map = new mapboxgl.Map({
-       container: 'map',
-       style: 'mapbox://styles/mapbox/streets-v9',
-       zoom: 0
-   });
+    /*Initializing Map*/
+    const ACCESS_TOKEN = 'pk.eyJ1IjoiYXVzdGtlIiwiYSI6ImNpd21uZTB1bDAwNm8yenF4ZmtlbjkzenUifQ.CohFKxWoYGrFXQDoRvZWag';
+    mapboxgl.accessToken = ACCESS_TOKEN;
+    var map = new mapboxgl.Map({
+      container: 'map',
+      style: 'mapbox://styles/mapbox/streets-v9',
+      zoom: 0
+    });
 
-   var url = 'https://wanderdrone.appspot.com/';
-   map.on('load', function () {
-       window.setInterval(function() {
-           map.getSource('movement').setData(url);
-       }, 50);
+    var url = 'https://wanderdrone.appspot.com/';
+    map.on('load', function() {
+      window.setInterval(function() {
+        map.getSource('movement').setData(url);
+      }, 50);
 
-       map.loadImage('../assets/icon/bus-top-view.png', function(error, image) {
-         if (error) throw error;
-       map.addSource('movement', { type: 'geojson', data: url });
-       console.log(map)
-       map.addImage('bus', image);
-       map.addLayer({
-           "id": "movement",
-           "type": "symbol",
-           "source": "movement",
-           "layout": {
-               "icon-image": "bus",
-               "icon-size": 0.15
+      map.loadImage('../assets/icon/bus-top-view.png', function(error, image) {
+        if (error) throw error;
+        map.addSource('movement', { type: 'geojson', data: url });
+        map.addImage('bus', image);
+        map.addLayer({
+          "id": "movingBus",
+          "type": "symbol",
+          "source": "movement",
+          "layout": {
+            "icon-image": "bus",
+            "icon-size": 0.15,
+            "icon-rotate": 20
 
-           }
-       });
-   });
- });
+          }
+        });
+      });
+    });
 
-}
+  }
 
 }
